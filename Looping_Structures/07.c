b687ac44dc5291de556e73dd2ff7cd5bac039218
ElonFreeman <zhanghao112358@outlook.com>
2025-11-15 21:19:33 +0800

    Update 07.c

--- Looping_Structures/07.c
+++ Looping_Structures/07.c
@@ -3,15 +3,10 @@
 
 
 int main()
-
 {
-
     char strings;
-
     int letters=0,space=0,nums=0,other=0;
-
-
-
+    
     while((strings=getchar())!='\n')
 
     {
@@ -27,9 +22,7 @@ int main()
         else if(strings==' ')
 
         {
-
             space++;
-
         }
 
         else if((strings>='0'&&strings<='9'))
