73321c21619febce81b33582a228eea1d06d08be
ElonFreeman <zhanghao112358@outlook.com>
2026-03-12 17:59:29 +0800

    ds exp

---
+++ data_structure_experiments/04.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct contacts
+{
+    string name;
+    int age;
+    string phone_number;
+};
+
+int main(void)
+{
+    struct contacts info[50]={};
+
+    
+    return 0;
+}
