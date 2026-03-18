624c43b86141bd2ba0f286fa58c5c4ed729e5215
ElonFreeman <zhanghao112358@outlook.com>
2026-03-18 19:48:03 +0800

    ds0318hw

---
+++ data_structure_experiments/20260318/structs/02.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct infos
+{
+    string name;
+    int stu_id;
+    int score;
+
+};
+
+int main(void)
+{
+    int n=0;
+    cin >> n;
+    vector<infos> stu_info(n);
+    for(infos &traverse:stu_info)
+    {
+        
+    }
+    return 0;
+}
