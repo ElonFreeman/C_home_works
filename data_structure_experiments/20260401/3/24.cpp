575497c987659b4abee5cb52dcc505318d62d3d4
ElonFreeman <zhanghao112358@outlook.com>
2026-04-04 13:18:27 +0800

    0404

--- data_structure_experiments/20260401/3/24.cpp
+++ data_structure_experiments/20260401/3/24.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main(void)
 {
-    
+    int n;
+    cin >> n;
+    vector<int> arr(n,0);
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr.at(i);
+    }
+
+    int m1,m2;
+    cin >> m1 >> m2;
+
+    for(int i=0;i<n;i++)
+    {
+        if(arr.at(i)==m1||arr.at(i)==m2)
+        {
+            
+        }
+    }
     return 0;
 }
