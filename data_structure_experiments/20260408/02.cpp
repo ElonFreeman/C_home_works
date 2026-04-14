f9730acffbac6d642c6357c0f3c73773b6a1e057
ElonFreeman <zhanghao112358@outlook.com>
2026-04-14 19:59:44 +0800

    26041401

--- data_structure_experiments/20260408/02.cpp
+++ data_structure_experiments/20260408/02.cpp
@@ -1,22 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct quelist
 {
-    int head,tail;
+    int head, tail;
     int people[10];
 };
 
 int main(void)
 {
-    quelist male={0,9,{1,2,3,4,5,6,7,8,9,10}},female={0,4,{1,2,3,4,5}};
+    quelist male = {0, 9, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}, female = {0, 4, {1, 2, 3, 4, 5}};
 
-    int i=0;
-    while(i<20)
+    int i = 0;
+    while (i < 20)
     {
         cout << male.people[male.head] << ' ' << female.people[female.head] << endl;
-        male.head=(male.head+10)%10+1,female.head=(female.head+5)%5+1;
-        male.tail=(male.tail+10)%10+1,female.tail=(female.tail+5)%5+1;
+        male.head = (male.head + 10 + 1) % 10 , female.head = (female.head + 5 + 1) % 5;
         i++;
     }
 
