15362af2e5ee278538b973bbfddcb4bf4555c329
ElonFreeman <zhanghao112358@outlook.com>
2026-04-23 17:20:59 +0800

    26042301

---
+++ data_structure_experiments/20260422/9/02.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct parenth
+{
+    int top=-1;
+    vector<char> str_left; 
+};
+
+void check(string str)
+{
+    parenth paren;
+    while(size(paren.str_left))
+    {
+        
+    }
+
+}
+
+int main(void)
+{
+    string str; cin >> str;
+    check(str);
+
+    return 0;
+}
