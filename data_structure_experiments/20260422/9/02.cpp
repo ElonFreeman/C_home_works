d9c0c7fa79779df4937693af6ccaf3b1d24e8263
ElonFreeman <zhanghao112358@outlook.com>
2026-04-24 19:32:12 +0800

    26042401

--- data_structure_experiments/20260422/9/02.cpp
+++ data_structure_experiments/20260422/9/02.cpp
@@ -10,11 +10,40 @@ struct parenth
 void check(string str)
 {
     parenth paren;
-    while(size(paren.str_left))
+    for(int i=0;i<size(paren.str_left);i++)
     {
-        
+       if(str[i]=='(' || str[i]=='[' || str[i]=='{')
+       {
+            paren.str_left.push_back(str[i]);
+            paren.top++;
+       }
+       else if(str[i]==')' || str[i]==']' || str[i]=='}')
+       {
+            if(str[paren.top]=='(' && str[i]==')')
+            {
+                paren.str_left.pop_back();
+                paren.top--;
+            }
+            else if(str[paren.top]=='[' && str[i]==']')
+            {
+                paren.str_left.pop_back();
+                paren.top--;
+            }
+            else if(str[paren.top]=='{' && str[i]=='}')
+            {
+                paren.str_left.pop_back();
+                paren.top--;
+            }
+            else
+            {
+                cout << "false";
+                return;
+            }
+       }
     }
-
+    
+    cout << "true";
+    return;
 }
 
 int main(void)
