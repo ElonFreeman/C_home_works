9969fb92a05f0695db0ad18ed7ad0c2abaf3013e
ElonFreeman <zhanghao112358@outlook.com>
2026-04-29 21:04:55 +0800

    26042901

--- data_structure_experiments/20260422/9/02.cpp
+++ data_structure_experiments/20260422/9/02.cpp
@@ -1,55 +1,100 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-struct parenth
-{
-    int top=-1;
-    vector<char> str_left; 
-};
+#define MAX_SIZE 100
 
-void check(string str)
-{
-    parenth paren;
-    for(int i=0;i<size(paren.str_left);i++)
-    {
-       if(str[i]=='(' || str[i]=='[' || str[i]=='{')
-       {
-            paren.str_left.push_back(str[i]);
-            paren.top++;
-       }
-       else if(str[i]==')' || str[i]==']' || str[i]=='}')
-       {
-            if(str[paren.top]=='(' && str[i]==')')
-            {
-                paren.str_left.pop_back();
-                paren.top--;
-            }
-            else if(str[paren.top]=='[' && str[i]==']')
-            {
-                paren.str_left.pop_back();
-                paren.top--;
-            }
-            else if(str[paren.top]=='{' && str[i]=='}')
-            {
-                paren.str_left.pop_back();
-                paren.top--;
-            }
-            else
-            {
-                cout << "false";
-                return;
-            }
-       }
+typedef struct {
+    char data[MAX_SIZE];
+    int top;
+} SeqStack;
+
+void Push(SeqStack *s, char x) {
+    if (s->top == MAX_SIZE - 1) {
+        printf("栈满\n");
+        return;
     }
-    
-    cout << "true";
-    return;
+    s->top++;
+    s->data[s->top] = x;
 }
 
-int main(void)
-{
-    string str; cin >> str;
-    check(str);
+char Pop(SeqStack *s) {
+    if (s->top == -1) {
+        printf("栈空\n");
+        return -1;
+    }
+    char x = s->data[s->top];
+    s->top--;
+    return x;
+}
 
+char GetTop(SeqStack *s) {
+    if (s->top != -1)
+        return s->data[s->top];
+    return -1;
+}
+
+int Empty(SeqStack *s) {
+    if (s->top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int BracketMatch(char *str) {
+    SeqStack stack;
+    stack.top = -1;
+    int i; 
+    char ch;
+    for (i = 0; i < strlen(str); i++) {
+        ch = str[i];
+        switch(ch) {
+            case '(':
+            case '[':
+            case '{':
+                /***********Begin***********/
+                Push(&stack, ch);
+                /************End************/
+                break;
+            case ')':
+                /************Begin************/
+                if (Empty(&stack) || GetTop(&stack) != '(')
+                    return 0;
+                else
+                    Pop(&stack);
+                /************End************/
+                break;
+            case ']':
+                /***********Begin***********/
+                if (Empty(&stack) || GetTop(&stack) != '[')
+                    return 0;
+                else
+                    Pop(&stack);
+              /************End************/
+                break;
+            case '}':
+                /***********Begin***********/
+                if (Empty(&stack) || GetTop(&stack) != '{')
+                    return 0;
+                else
+                    Pop(&stack);
+                /************End************/
+                break;
+            default:
+                break;
+        }
+    }
+    if (Empty(&stack))
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    char str[MAX_SIZE];
+    fgets(str, MAX_SIZE, stdin);
+    if (BracketMatch(str))
+        printf("true\n");
+    else
+        printf("false\n");
     return 0;
 }
---
+++ data_structure_experiments/20260423/0102.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string mainstr="asdfghjklpoiuyt",substr1="hjklpo",substr2="hqklbo";
+
+void match(string mainstr,string substr1,string substr2)
+{
+    
+}
+
+int main(void)
+{
+
+    return 0;
+}
