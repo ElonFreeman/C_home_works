41ccc94bae4ea6ed777e5cb18f7513a75ab46b17
ElonFreeman <zhanghao112358@outlook.com>
2026-06-09 17:06:29 +0800

    260901

--- data_structure_experiments/20260528/graph_storage_adjacency_list.cpp
+++ data_structure_experiments/20260528/graph_storage_adjacency_list.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 vector<bool> access_lable;
 int num_vertex=0,num_edge=0;
-    vector<vertexnode> nodes(num_vertex);
 
 struct edge
 {
@@ -17,33 +16,60 @@ struct vertexnode
     edge *first_edge,*end;
 };
 
-void BFS(vertexnode *begin)
+void BFS(vertexnode *vertexlist)
 {
     deque<vertexnode> bfs_queue;
+    int vertex_index=0;
     while(size(bfs_queue))
     {
-        bfs_queue.push_back(*begin);
-        edge *current=(*begin).first_edge;
+        bfs_queue.push_back(vertexlist[vertex_index]);
+        edge *current=(vertexlist[vertex_index]).first_edge;
         while(current!=nullptr)
         {
-            bfs_queue.push_back(begin[(*current).index]);
-            current=current->next;
+            if(access_lable[current->index]==true)
+            {
+                current=current->next;
+                continue;
+            }
+            else
+            {
+                bfs_queue.push_back(vertexlist[current->index]);
+                current=current->next;
+                access_lable[current->index]=true;
+            }
         }
+        cout << bfs_queue.begin()->data << ' ';
+        bfs_queue.pop_front();
+        vertex_index++;
     }
     
 }
 
 void DFS(vertexnode *begin)
 {
-    
+    for(int i=0;i<num_vertex;i++)
+    {
+        cout << begin[i].data << ' ';
+        access_lable[i]=true;
+        
+        if(begin[i].first_edge)
+        {
+            DFS(&begin[begin[i].first_edge->index]);
+
+        }
+    }
+
 }
 
 int main(void)
 {
     cin >> num_vertex >> num_edge;
+    vector<vertexnode> nodes(num_vertex);
     for(int i=0;i<num_vertex;i++)
     {
-        cin >> nodes[i].data,nodes[i].first_edge=nullptr,nodes[i].end=nodes[i].first_edge;
+        cin >> nodes[i].data,
+        nodes[i].first_edge=nullptr,
+        nodes[i].end=nodes[i].first_edge;
         access_lable.push_back(false);
     }
 
